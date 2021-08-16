a74eaa4352270f6e7cc2fe12ef7640b37964b9df
Rob Fletcher <rfletcher@netflix.com>
2021-08-16 06:27:41 -0700

    skeleton css

--- src/App.svelte
+++ src/App.svelte
@@ -75,38 +75,21 @@
 		<dt>Weapons</dt>
 		<dd>
 			<ul>
-			{#each traveler.inventory.weapons as { description, type } }
-				<li>{description} <small>{type}</small></li>
-			{/each}
+				{#each traveler.inventory.weapons as { description, type } }
+					<li>{description} <small>{type}</small></li>
+				{/each}
 			</ul>
 		</dd>
 		<dt>Armor</dt>
-		<ul>
-			{#each traveler.inventory.armor as { description, type } }
-				<li>{description} <small>{type}</small></li>
-			{/each}
+		<dd>
+			<ul>
+				{#each traveler.inventory.armor as { description, type } }
+					<li>{description} <small>{type}</small></li>
+				{/each}
 			</ul>
+		</dd>
 	</dl>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
